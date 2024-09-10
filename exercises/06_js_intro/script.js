3546b38876d96ecd4206d3f9f7109e28a21935b6
JacobTipps43 <jacobtippett43@gmail.com>
2024-09-10 12:51:31 -0400

    In class example

--- exercises/06_js_intro/script.js
+++ exercises/06_js_intro/script.js
@@ -3,6 +3,8 @@ const dostuff = () =>{
 };
 
 const myButton = document.getElementById("btn-click");
+const sayGoodbye = document.getElementById("say_goodbye");
+const hide = document.getElementById("hide");
 //myButton.onclick = dostuff;
 
 myButton.onclick = () =>{
@@ -10,5 +12,17 @@ myButton.onclick = () =>{
     document.getElementById("stuff").classList.add("special");
 };
 
+sayGoodbye.onclick = () =>{
+    document.getElementById("message").innerHTML = "GOODBYE EVERYONE!!!";
+    document.getElementById("stuff").classList.remove("special");
+}
+
+hide.onclick = () =>{
+    document.getElementById("column_1").classList.add("hide");
+}
+
 //Add a second button, called say goodbye
 //When clicked change the text to goodbye everyone, and remove the special styles
+//if you finish that make a button that hides a column
+//if you get that add a button that will animate 'stuff' when clicked
+//then you can have a button that will stop the animation
