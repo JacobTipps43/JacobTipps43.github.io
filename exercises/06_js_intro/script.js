960baf50868b3ae77bc052df019ba90643ae423c
JacobTipps43 <jacobtippett43@gmail.com>
2024-09-10 12:32:22 -0400

    In class example

--- exercises/06_js_intro/script.js
+++ exercises/06_js_intro/script.js
@@ -1,1 +1,14 @@
+const dostuff = () =>{
+    alert("Hello World");
+};
+
 const myButton = document.getElementById("btn-click");
+//myButton.onclick = dostuff;
+
+myButton.onclick = () =>{
+    document.getElementById("message").innerHTML = "Hello Everyone!";
+    document.getElementById("stuff").classList.add("special");
+};
+
+//Add a second button, called say goodbye
+//When clicked change the text to goodbye everyone, and remove the special styles
