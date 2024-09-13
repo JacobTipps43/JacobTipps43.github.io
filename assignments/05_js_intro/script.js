d627fa73bf006d003af37fef02a7ddc8541e6901
JacobTipps43 <jacobtippett43@gmail.com>
2024-09-13 16:23:23 -0400

    Figuring out how the slider works

--- assignments/05_js_intro/script.js
+++ assignments/05_js_intro/script.js
@@ -1,8 +1,13 @@
 let count = 0;
+let slide = 30;
 
 const counterDisplay = document.getElementById("counter");
 const buttonCounter = document.getElementById("btn-counter");
 const buttonImage = document.getElementById("btn-img");
+
+const slider = document.getElementById('myRange');
+const output = document.getElementById('sliderValue');
+const image = document.getElementById('myImage');
 
 buttonCounter.onclick = () =>{
     count++;
@@ -11,4 +16,9 @@ buttonCounter.onclick = () =>{
 
 buttonImage.onclick = () =>{
     location.reload()
+};
+
+slider.oninput = function() {
+    output.textContent = this.value;
+    image.style.paddingLeft = this.value + 'px';
 };
