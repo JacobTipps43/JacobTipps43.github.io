f864a6ff001e0ffb886b3805c9d19cebbf3ca6b7
JacobTipps43 <jacobtippett43@gmail.com>
2024-09-20 15:08:25 -0400

    Finished assignment

--- assignments/06_js_cont/script.js
+++ assignments/06_js_cont/script.js
@@ -1,6 +1,9 @@
+let red1 = 0;
+
 const navButton = document.getElementById("nav-button");
 const mainNav = document.getElementById("main-nav");
 const buttons = document.querySelectorAll(".buttons");
+const slider = document.getElementById('myRange');
 
 navButton.onclick = () => {
     document.getElementById("main-nav").classList.toggle("hidden-small");
@@ -16,6 +19,7 @@ navButton.onclick = () => {
 const handleButtonClick = (event) =>{
     const button_event = event.target.dataset.action;
     const messageP = document.getElementById("images");
+
     console.log(`Button clicked: ${button_event}`);
 
     if(button_event == "action1"){
@@ -35,3 +39,19 @@ const handleButtonClick = (event) =>{
 buttons.forEach((button) => {
     button.onclick = handleButtonClick;
 });
+
+slider.oninput = function() {
+    const message = document.getElementById("message")
+    red1 = this.value;
+    document.querySelector(":root").style.setProperty("--red1", red1);
+    console.log(red1)
+    if(red1 >=0 && red1 <75){
+        message.innerHTML = "Dark";
+    }
+    else if(red1 >=75 && red1 <180){
+        message.innerHTML = "Chill";
+    }
+    else if(red1 >=180){
+        message.innerHTML = "Bright";
+    }
+};
