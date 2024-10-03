8369d81a528812436a515c8c69be477458fdbd3c
JacobTipps43 <jacobtippett43@gmail.com>
2024-10-03 12:08:03 -0400

    Adding stuff to script.js

---
+++ exercises/10_classes/script.js
@@ -0,0 +1,26 @@
+class Dog {
+    constructor(title, breed, color, age, size, pic){
+        this.title = title;
+        this.breed = breed;
+        this.color = color;
+        this.age = age;
+        this.size = size;
+        this.pic = pic;
+    }
+
+    get item(){
+        const section = document.createElement("section");
+        section.classList.add("dog");
+        return section;
+    }
+}
+
+const dogs = [];
+//const myDoc = new Dog("molly", "pit bull", "brown", 4, "xs", "images/molly.jpg");
+dogs.push(new Dog("Coco", "Morkie", "Black", 5, "small", "yorkie.jpg"));
+dogs.push(new Dog("Sam", "Golden", "Yellow", 1, "med", "golden-retriever.jpg"));
+dogs.push(new Dog("Gerald","Pit Bull", "White", 3, "lg", "pitt-bull.jpg"));
+
+dogs.forEach((dog)=>{
+    document.getElementById("dog-list").append(dog.item);
+});
