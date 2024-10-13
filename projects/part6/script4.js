e132c202da68ab751ca66b6c0674f26141b94d2f
JacobTipps43 <jacobtippett43@gmail.com>
2024-10-13 17:11:51 -0400

    Adding changes to Talismans

--- projects/part6/script4.js
+++ projects/part6/script4.js
@@ -6,7 +6,7 @@ const getTalismans = async() => {
     try{
         const response = await fetch("https://jacobtipps43.github.io/projects/part6/EldenRing.json");
         const data = await response.json();
-        console.log("Weapons loaded");
+        console.log("Talismans loaded");
         return data;
     }catch(error){
         console.log(error);
@@ -37,6 +37,98 @@ const showTalismans = async() => {
         const p2 = document.createElement("p");
         p2.innerHTML = talisman.description
         section.append(p2);
-    }
+    });
+
+    Talis.Talismans[0].DexterityTalismans.forEach((talisman) =>{
+        const section = document.createElement("section");
+        section.classList.add("Talis");
+        section.classList.add("one");
+        document.getElementById("Talismans-dexterity").append(section);
+
+        //create the heading
+        const p1 = document.createElement("p");
+        p1.innerHTML = talisman.name;
+        section.append(p1);
+
+        //Talisman image
+        const img = document.createElement("img");
+        img.src = talisman.img;
+        img.id = "talismanimages"
+        section.append(img);
+
+        //Talisman description
+        const p2 = document.createElement("p");
+        p2.innerHTML = talisman.description
+        section.append(p2);
+    });
+
+    Talis.Talismans[0].MageTalismans.forEach((talisman) =>{
+        const section = document.createElement("section");
+        section.classList.add("Talis");
+        section.classList.add("one");
+        document.getElementById("Talismans-mage").append(section);
+
+        //create the heading
+        const p1 = document.createElement("p");
+        p1.innerHTML = talisman.name;
+        section.append(p1);
+
+        //Talisman image
+        const img = document.createElement("img");
+        img.src = talisman.img;
+        img.id = "talismanimages"
+        section.append(img);
+
+        //Talisman description
+        const p2 = document.createElement("p");
+        p2.innerHTML = talisman.description
+        section.append(p2);
+    });
+
+    Talis.Talismans[0].ArcaneTalismans.forEach((talisman) =>{
+        const section = document.createElement("section");
+        section.classList.add("Talis");
+        section.classList.add("one");
+        document.getElementById("Talismans-arcane").append(section);
+
+        //create the heading
+        const p1 = document.createElement("p");
+        p1.innerHTML = talisman.name;
+        section.append(p1);
+
+        //Talisman image
+        const img = document.createElement("img");
+        img.src = talisman.img;
+        img.id = "talismanimages"
+        section.append(img);
+
+        //Talisman description
+        const p2 = document.createElement("p");
+        p2.innerHTML = talisman.description
+        section.append(p2);
+    });
+
+    Talis.Talismans[0].FaithTalismans.forEach((talisman) =>{
+        const section = document.createElement("section");
+        section.classList.add("Talis");
+        section.classList.add("one");
+        document.getElementById("Talismans-faith").append(section);
+
+        //create the heading
+        const p1 = document.createElement("p");
+        p1.innerHTML = talisman.name;
+        section.append(p1);
+
+        //Talisman image
+        const img = document.createElement("img");
+        img.src = talisman.img;
+        img.id = "talismanimages"
+        section.append(img);
+
+        //Talisman description
+        const p2 = document.createElement("p");
+        p2.innerHTML = talisman.description
+        section.append(p2);
+    });
 }
 showTalismans();
