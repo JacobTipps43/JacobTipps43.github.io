2b3fb385cb31c58798c3470a96355465e6b5d6c5
JacobTipps43 <jacobtippett43@gmail.com>
2024-10-13 15:57:09 -0400

    Adding mage Weapons

--- projects/part6/script2.js
+++ projects/part6/script2.js
@@ -61,6 +61,29 @@ const showWeapons = async() => {
         p2.innerHTML = weapon.description;
         section.append(p2);
     });
+
+    Weps.Weapons[0].MagicWeapons.forEach((weapon) =>{
+        const section = document.createElement("section");
+        section.classList.add("Weps");
+        section.classList.add("one");
+        document.getElementById("Weapons-magic").append(section);
+
+        //create the heading
+        const p1 = document.createElement("p");
+        p1.innerHTML = weapon.name;
+        section.append(p1);
+
+        //Weapon image
+        const img = document.createElement("img");
+        img.src = weapon.img;
+        img.id = "weaponimages"
+        section.append(img);
+
+        //Weapon description
+        const p2 = document.createElement("p");
+        p2.innerHTML = weapon.description;
+        section.append(p2);
+    });
 }
 
 showWeapons();
