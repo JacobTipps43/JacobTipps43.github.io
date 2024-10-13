ff8a9a5be842e3d13e7eae2cd96ae792abb99203
JacobTipps43 <jacobtippett43@gmail.com>
2024-10-13 13:55:02 -0400

    Adding json

--- projects/part6/script2.js
+++ projects/part6/script2.js
@@ -1,3 +1,39 @@
 document.querySelector("#toggle-nav").onclick = () => {
     document.getElementById("nav-items").classList.toggle("hidden-small");
 };
+
+const getWeapons = async() => {
+    try{
+        return (await fetch("EldenRing.json")).json();
+        console.log("Weapons loaded");
+    }catch(error){
+        console.log(error);
+    }
+}
+
+const showWeapons = async() => {
+    const Weps = await getWeapons();
+
+    Weps.Weapons.StrengthWeapons.forEach((Weapon)=>{
+        const section = document.createElement("section");
+        section.classList.add("Weps");
+        document.getElementById("Weapons").append(section);
+
+        //create the heading
+        const p1 = document.createElement("p");
+        p1.innerHTML = Weps.name;
+        section.append(p1);
+
+        //Weapon image
+        const img = document.createElement("img");
+        img.src = Weps.image;
+        section.append(img);
+
+        //Weapon description
+        const p2 = document.createElement("p");
+        p2.innerHTML = Weps.description;
+        section.append(p2);
+    });
+}
+
+showWeapons();
