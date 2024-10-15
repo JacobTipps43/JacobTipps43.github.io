c14e8b857a7f655e388f464ba99152580bd19bbd
JacobTipps43 <jacobtippett43@gmail.com>
2024-10-15 12:14:31 -0400

    Adding start of in class exercise 12

---
+++ exercises/12_jasper/form.js
@@ -0,0 +1,21 @@
+document.getElementById('form-raccoon').onsubmit = (e) =>{
+    e.preventDefault();//don't go to the action ... don't refresh the page
+    
+    const form = e.target;
+
+    const raccoonName = form.elements["raccoon-name"].value;
+    const demeanor = form.elements["demeanor"].value;
+    const termsChecked = form.elements["terms"].checked;
+
+    console.log(raccoonName);
+};
+
+const getRadioValue = (radioName) => {
+    const radios = document.getElementsByName(radioName);
+
+    for(let i in radios){
+        if(radios[i].checked){
+            return radios[i].value;
+        }
+    }
+}
